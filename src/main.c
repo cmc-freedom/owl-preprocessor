5f50a6e680d64544d948d451e1e59b9e749300a6
vslutov <vslutov@yandex.ru>
2014-01-14 18:38:56 +0400

    add backtrace

--- src/main.c
+++ src/main.c
@@ -6,6 +6,88 @@
 const char *USAGE =
 "usage: owl FILE ...\n";
 
+typedef char *String;
+
+typedef struct
+{
+  String string;
+  int line;
+}
+StackRecord;
+
+struct
+{
+  StackRecord *data;
+  size_t real_size, size;
+} stack;
+
+void
+backtrace (void)
+{
+  printf("BACKTRACE:\n");
+  for (size_t i = 0; i < stack.size; ++ i)
+    printf("    %s (%d line)\n", stack.data[i].string, stack.data[i].line);
+  printf("\n");
+}
+
+void
+stop (const String string)
+{
+  backtrace();
+  printf("%s\n", string);
+  exit(1);
+}
+
+void
+append (const String string, int line)
+{
+  size_t length = strlen(string);
+  stack.data[stack.size].line = line;
+  stack.data[stack.size].string = (String) malloc(length + 1);
+  if (stack.data[stack.size].string == NULL)
+    {
+      backtrace();
+      printf("Cannot realloc memory for append string \"%s\" "
+             "to function stack.", string);
+      exit(1);
+    }
+  else
+    strcpy(stack.data[stack.size].string, string);
+
+  ++ stack.size;
+  if (stack.size == stack.real_size)
+    {
+      stack.real_size <<= 1;
+      StackRecord *data = (StackRecord *)
+                            realloc(stack.data,
+                                    sizeof(StackRecord) * stack.real_size);
+      if (data == NULL)
+        {
+          -- stack.size;
+          stop("Cannot realloc memory for function stack.");
+        }
+      else
+        stack.data = data;
+    }
+}
+
+#define begin(string) append(string, __LINE__)
+
+void
+end (void)
+{
+  free(stack.data[stack.size].string);
+  -- stack.size;
+}
+
+void
+init (void)
+{
+  stack.data = (StackRecord *) malloc(sizeof(StackRecord) * 1);
+  stack.real_size = 1;
+  stack.size = 0;
+}
+
 void
 preprocess_file (const char *filename)
 {
@@ -46,10 +128,12 @@ preprocess_file (const char *filename)
 int
 main(int argc, char **argv)
 {
-  if (argc == 1)
-    printf("%s", USAGE);
-  else
-    for (int i = 1; i < argc; ++i)
-      preprocess_file(argv[i]);
+  init();
+  begin("main");
+  begin("Hello, World!");
+  backtrace();
+  end();
+  backtrace();
+  end();
 }
 
